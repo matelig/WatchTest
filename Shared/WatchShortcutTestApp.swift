a38beeb625b9e0fbe0462aca54c293de155fe3b7
Mateusz Ligus <mateusz.ligus@tedee.com>
2021-11-04 12:36:52 +0100

    Initial Commit

---
+++ Shared/WatchShortcutTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchShortcutTestApp.swift
+//  Shared
+//
+//  Created by Mateusz Ligus on 04/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct WatchShortcutTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
